7af56c83325b1819079d5828dd3d9d49593ca5ff
241007 <241007@std.hi-joho.ac.jp>
2025-06-11 09:27:17 +0900

    第9章の教科書コード

--- src/Hero.java
+++ src/Hero.java
@@ -2,41 +2,35 @@ public class Hero {
     String name;
     int hp;
     Sword sword;
-
-    public Hero(String name){//戻り値省略でクラス名と一緒の名前にするとコンストラクタができる。(初期化に使う)
+    public void attack() {
+        System.out.println(this.name + "は攻撃した！");
+        System.out.println("敵に5ポイントのダメージをあたえた！");
+    }
+    // 引数なし
+    public Hero() {//同じ名前
+        this.hp = 100;
+        this.name = "ダミー";
+        //this(100, "ダミー");
+        System.out.println("引数なし");
+    }
+    // 引数あり String
+    public Hero(String name) {//同じ名前
         this.hp = 100;
         this.name = name;
+        //this(100, name);
+        System.out.println("引数あり String");
     }
-    public Hero(){
-        this("ダミー");
+    // 引数あり int
+    public Hero(int hp) {//同じ名前
+        this.hp = hp;
+        this.name = "ダミー";
+        //this(hp, "ダミー");
+        System.out.println("引数あり int");
     }
-    public void sleep(){
-        this.hp = 100;
-        System.out.println(this.name+"は、眠って回復した！");
-    }
-    public void sit(int sec){
-        this.hp += sec;
-        System.out.println(this.name+"は、"+sec+"秒座った！");
-        System.out.println("HPが"+sec+"ポイント回復した");
-    }
-    public void slip(){
-        this.hp -= 5;
-        System.out.println(this.name+"は、転んだ！");
-        System.out.println("5のダメージ！");
-    }
-    public void run(){
-        System.out.println(this.name+"は、逃げ出した！");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは"+this.hp+"でした");
-    }
-    public void attack(Matango m){
-        int currHp = m.hp;
-        m.hp -= 20;
-        System.out.println("マタンゴ"+m.suffix+"に20のダメージ");
-        System.out.println("マタンゴ"+m.suffix+"のHP:"+m.hp+"/"+currHp);
-    }
-    public void attack(){
-        System.out.println(this.name+"は攻撃した！");
-        System.out.println("敵に5ポイントのダメージをあたえた！");
+    // 引数2つ
+    public Hero(int hp, String name) {//同じ名前
+        this.hp = hp;
+        this.name = name;
+        System.out.println("引数2つ");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        Sword s = new Sword();
-        s.name = "炎の剣";
-        s.damage = 10;
-        Hero h = new Hero();
-        h.name = "ミナト";
-        h.hp = 100;
-        h.sword = s;
-        System.out.println("現在の武器は"+h.sword.name);
-        */
-
-        Hero h1 = new Hero();
-        System.out.println(h1.hp);
-        System.out.println(h1.name);
-        Hero h2 = new Hero("アサカ");
-        Wizard w = new Wizard("スガワラ");
-        w.heal(h1);
-        w.heal(h2);
-        w.heal(h2);
-
-        /*
-        Matango m1 = new Matango();
-        m1.hp = 50;
-        m1.suffix = 'A';
-
-        Matango m2 = new Matango();
-        m2.hp = 48;
-        m2.suffix = 'B';
-
-        h.attack(m1);
-        if(m1.hp <= 0){
-            m1.run();
-        }else{
-            m1.attack(h);
-            h.run();
-        }
-        */
+        // 引数なし
+        Hero h = new Hero();//同じ名前
+        System.out.println(h.name + "のHPは" + h.hp + "です");
+        // 引数あり String
+        Hero h1 = new Hero("ミナト");//同じ名前
+        System.out.println(h1.name + "のHPは" + h1.hp + "です");
+        // 引数あり int
+        Hero h2 = new Hero(999);//同じ名前
+        System.out.println(h2.name + "のHPは" + h2.hp + "です");
+        // 引数2つ
+        Hero h3 = new Hero(999, "ミナト");//同じ名前
+        System.out.println(h3.name + "のHPは" + h3.hp + "です");
     }
 }
